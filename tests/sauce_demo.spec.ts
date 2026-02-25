dd9adfbe5f34d78d6c11b935bc69fde7fd22b858
PaulAlanS <paulalanmisterek@gmail.com>
2026-02-25 15:28:15 +0530

    Test for Sauce Demo login

---
+++ tests/sauce_demo.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('login check', async({page})=>{
+    await page.goto('https://sauce-demo.myshopify.com/cart');
+    await page.getByRole('link',{name :'Log In'}).click();
+    await page.locator('#customer_email').fill('bdnu@gmail.com');
+    await page.getByRole('textbox',{name : 'Password'}).fill('1N2B3V');
+    await page.getByRole('button', {name : 'Sign In'}).click();
+    await expect(page.locator('.accounts-title')).toHaveText('Your Account');
+})  
