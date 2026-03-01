89ffa790b34e9bb847f83a7d776e3266d6e776b5
PaulAlanS <paulalanmisterek@gmail.com>
2026-03-01 16:47:53 +0530

    Cucumber istalled

--- tests/redbus_demo.spec.ts
+++ tests/redbus_demo.spec.ts
@@ -4,10 +4,12 @@ test('booking bus in redbus', async({page})=>{
     await page.goto('https://www.redbus.in/');
     
     await page.locator('#srcinput').fill('Chennai');
-    await page.locator('div[role = "option"]').first().click();
+    await page.getByRole('option').first().click();
 
-    
     await page.locator('#destinput').fill('Madurai');
     await page.locator('div[role = "option"]').first().click();
+
+    //await page.locator('span',{hasText :'Date of Journey'}).click();
+    //await page.getByRole('combobox',{name : 'Search for Tomorrow'}).click();
 
 })
